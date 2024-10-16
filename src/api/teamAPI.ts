4e5299b967695ca02c70c94b7de16df97efe07dd
Yordan2000 <yordanviafara9@gmail.com>
2024-10-15 22:12:13 -0500

    fix/bug error en build

--- src/api/teamAPI.ts
+++ src/api/teamAPI.ts
@@ -16,7 +16,7 @@ export async function findUserByEmail({
 }) {
   try {
     const url = `/projects/${projectId}/team/find`;
-    const { data } = await api.post<string>(url, formData);
+    const { data } = await api.post(url, formData);
     return data;
   } catch (error) {
     if (isAxiosError(error) && error.response) {
